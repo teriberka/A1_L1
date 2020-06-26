0bb776976791df96b22c67bcabedb10509c28ccb
teriberka <box4trash@inbox.ru>
2020-06-26 16:47:23 +0300

    add hometask commit

--- src/ru/gb/a1/l1/hometask/Main.java
+++ src/ru/gb/a1/l1/hometask/Main.java
@@ -1,5 +1,29 @@
 package ru.gb.a1.l1.hometask;
 
 public class Main {
-    
+    public static void main(String args[]){
+
+        System.out.println(exponentiation(5,2));
+
+        int[] testArray = {1,2,3,-4,5,-6,-5,7,8,9,0};
+
+        minElementArray(testArray);
+
+    }
+
+    // задание 1.1 | Возведение в степень
+    public static int exponentiation(int digit, int exponentiation){
+        return (int) Math.pow(digit, exponentiation);
+    }
+
+    // задание 1.2 | Поиск минимального элемента в массиве
+    public static int minElementArray(int[] arr){
+        int x = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(i);
+        }
+        return x;
+    }
+
 }
